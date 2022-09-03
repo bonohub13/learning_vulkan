c3755c16906612fa7afc1781d93bf087e222427f
Kensuke Saito <ken.saito.0813@gmail.com>
2022-09-03 14:45:17 +0900

    Added base for app

---
+++ src/hello_triangle_application.rs
@@ -0,0 +1,7 @@
+pub struct HelloTriangleTriangle {}
+
+impl HelloTriangleTriangle {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+mod hello_triangle_application;
+
 use vk_utils::constants::*;
+
+use hello_triangle_application::HelloTriangleTriangle;
 
 use winit::{
     dpi::PhysicalSize,
+    event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
@@ -14,11 +19,36 @@ fn main() {
         .with_min_inner_size(PhysicalSize::new(MINIMAL_WIDTH, MINIMAL_HEIGHT))
         .build(&event_loop)
         .unwrap();
+    let app = HelloTriangleTriangle::new();
 
+    // Application loop
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Poll;
 
         match event {
+            // Press the "erase window" button or press the escape key to kill
+            // app
+            Event::WindowEvent {
+                event:
+                    WindowEvent::CloseRequested
+                    | WindowEvent::KeyboardInput {
+                        input:
+                            KeyboardInput {
+                                state: ElementState::Pressed,
+                                virtual_keycode: Some(VirtualKeyCode::Escape),
+                                ..
+                            },
+                        ..
+                    },
+                ..
+            } => {
+                *control_flow = ControlFlow::Exit;
+            }
+            // Main event for app
+            Event::MainEventsCleared => {
+                // TODO Render here!
+            }
+            // Some other random events
             _ => (),
         }
     });
