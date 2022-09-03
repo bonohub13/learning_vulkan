fa6491a3587ef3234be7a2bee4cda1b9568b1459
Kensuke Saito <ken.saito.0813@gmail.com>
2022-09-03 14:30:42 +0900

    trashed all code due to fatal bug

---
+++ lib/constants.rs
@@ -0,0 +1,10 @@
+mod _constants {
+    pub const WIDTH: u32 = 800;
+    pub const HEIGHT: u32 = 600;
+    pub const MINIMAL_WIDTH: u32 = 100;
+    pub const MINIMAL_HEIGHT: u32 = 100;
+}
+
+pub use _constants::{HEIGHT, WIDTH};
+// minimal window size
+pub use _constants::{MINIMAL_HEIGHT, MINIMAL_WIDTH};
